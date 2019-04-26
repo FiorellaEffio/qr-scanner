d1108dac11f0db1533d000e7ded7da8820da711b
vbiaggi <vbiaggi10@gmail.com>
2019-04-26 17:42:59 -0500

    scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BarcodeScanner
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  products: any[] = [];
+  selectedProduct: any;
+  productFound:boolean = false;
 
+  constructor(private barcodeScanner: BarcodeScanner) {
+
+          this.products = [
+            {
+              "plu":"01234567895",
+              "name":"Gaming DDR5 RAM 16GB",
+              "price":76,
+              "desc":"Gaming DDR5 RAM 16GB PC-128000 For x64 PC"
+            },
+            {
+              "plu":"01234567898",
+              "name":"Intel Core i7 3.3GHz",
+              "price":200,
+              "desc":"Intel Core i7 3.3GHz L2 16MB 4.6ns"
+            }
+          ];
+          console.log(this.products);
+    }
+    scan() {
+      this.selectedProduct = {};
+      this.barcodeScanner.scan().then((barcodeData) => {
+        this.selectedProduct = barcodeData.text;
+        this.productFound = true;
+
+      }, (err) => {
+        console.log("no hay nada");
+      });
+    }
 }
